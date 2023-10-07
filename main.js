8cb5bcaea71daa56b78422687d1776b97c7f0609
Adeel Zaman <144605482+Adeel-Zaman-63@users.noreply.github.com>
2023-10-07 14:30:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+// ===========
+// NAVIGATION
+// ===========
+const mainNav = document.querySelector(".NAV_CONTAINER");
+const navTimes = document.querySelector("#navTimes");
+const navBars = document.querySelector("#navBar");
+
+
+navBars.addEventListener('click', () => {
+    if (!mainNav.classList.contains("show_nav")) {
+        mainNav.classList.add("show_nav")
+    }else{
+        mainNav.classList.remove("show_nav")
+    }
+})
+
+
+// ===========
+// BACK TOTOP
+// ===========
+const totop = document.querySelector(".TotopContainer");
+// console.log(totop);
+window.addEventListener("scroll", () => {
+    const scrollHgt = window.pageYOffset;
+    // console.log(scrollHgt);
+    if(scrollHgt > 500){
+        totop.classList.add("showtoTop");
+    }else{
+        totop.classList.remove("showtoTop");
+    }
+})
+
+
+// =============== 
+// SKILL SECTION
+// ===============
+const skillLeft_contaniner = document.querySelector('.skill_left');
+const skillRight_contaniner = document.querySelector('.skill_right');
+const skillbtns = document.querySelectorAll(".skillHead");
+const skillContainers = document.querySelectorAll('.skillContainer');
+const skillShow = document.querySelector('.skillShow');
+const one = document.querySelector('.skillOneContainer');
+const two = document.querySelector('.skillTwoContainer');
+const three = document.querySelector('.skillThreeContainer');
+const four = document.querySelector('.skillFourContainer');
+
+console.log(two.nextElementSibling);
+function showOne () {
+    
+    
+}
+
+
+skillbtns.forEach((skilllink) => {
+    skilllink.addEventListener("click", (e) => {
+        e.currentTarget;
+        if(skilllink.classList.contains('skillHeadOne')){
+            // showOne();
+        }
+        if(skilllink.classList.contains('skillHeadTwo')){
+            // showOne()
+        }
+        if(skilllink.classList.contains('skillHeadThree')){
+            // showOne()
+        }
+        if(skilllink.classList.contains('skillHeadFour')){
+        }
+    })
+})
